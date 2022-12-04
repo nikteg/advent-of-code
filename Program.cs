1d506358d723fbc5e7a3b86b49dcf83682ad6aec
Niklas Tegnander <niklas.tegnander@stenaline.com>
2022-12-04 19:46:45 +0100

    Shitty day 4 solution

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// adventofcode.Day1.Solution.Solve();
 // adventofcode.Day2.Solution.Solve();
-adventofcode.Day3.Solution.Solve();
+// adventofcode.Day3.Solution.Solve();
+adventofcode.Day4.Solution.Solve();
