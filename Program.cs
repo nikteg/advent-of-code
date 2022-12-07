7a91dfcb953db17e4cf8ea83be2b68b702da5f2f
Niklas Tegnander <niklas.tegnander@stenaline.com>
2022-12-07 20:22:18 +0100

    Day 7

--- Program.cs
+++ Program.cs
@@ -3,4 +3,5 @@
 // adventofcode.Day3.Solution.Solve();
 // adventofcode.Day4.Solution.Solve();
 // adventofcode.Day5.Solution.Solve();
-adventofcode.Day6.Solution.Solve();
+// adventofcode.Day6.Solution.Solve();
+adventofcode.Day7.Solution.Solve();
